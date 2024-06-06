ea679c2e0d712626e5eba19351cf3d40630c533f
Rio Edwards <rioredwards@gmail.com>
2024-06-06 11:47:35 -0700

    Test passing: The returned array should have strings that don't exceed maxLength when maxLength is not 1

--- wordWrap.ts
+++ wordWrap.ts
@@ -2,12 +2,14 @@ export function wrapWords(str: string, maxWidth: number, maxHeight: number): str
   // Check if string doesn't need to be wrapped
   if (str.length <= maxWidth) return str;
 
+  str = str.trim();
   let char: string = "";
   let word: string = "";
   let line: string = "";
   const lines: string[] = [];
   let spaceLeftInLine: number;
 
+  // Primary loop for wrapping words
   for (let i = 0; i < str.length; i++) {
     char = str[i];
 
@@ -33,7 +35,20 @@ export function wrapWords(str: string, maxWidth: number, maxHeight: number): str
     }
   }
 
+  // Account for last word
+  if (word.length > 0) {
+    spaceLeftInLine = maxWidth - line.length - (line.length && 1);
+    if (spaceLeftInLine - word.length >= 0) {
+      // Line has room for word
+      // Add space if line already has content, then add word
+      line += (line.length > 0 ? " " : "") + word;
+    }
+  }
+
+  // Account for last line
+  if (line.length > 0) {
+    lines.push(line);
+  }
+
   return lines;
 }
-
-// Start by just building words
